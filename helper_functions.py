675c68a4a23cfcf592b984f7dc0293f6cb47bebf
ciollonz <a.carmantini@ed.ac.uk>
2021-02-28 17:51:28 +0000

    Added lab 5 and solutions to 4

---
+++ helper_functions.py
@@ -0,0 +1,55 @@
+import openfst_python as fst
+
+def parse_lexicon(lex_file):
+    """
+    Parse the lexicon file and return it in dictionary form.
+    
+    Args:
+        lex_file (str): filename of lexicon file with structure '<word> <phone1> <phone2>...'
+                        eg. peppers p eh p er z
+
+    Returns:
+        lex (dict): dictionary mapping words to list of phones
+    """
+    
+    lex = {}  # create a dictionary for the lexicon entries (this could be a problem with larger lexica)
+    with open(lex_file, 'r') as f:
+        for line in f:
+            line = line.split()  # split at each space
+            lex[line[0]] = line[1:]  # first field the word, the rest is the phones
+    return lex
+
+def generate_symbol_tables(lexicon, n=3):
+    '''
+    Return word, phone and state symbol tables based on the supplied lexicon
+        
+    Args:
+        lexicon (dict): lexicon to use, created from the parse_lexicon() function
+        n (int): number of states for each phone HMM
+        
+    Returns:
+        word_table (fst.SymbolTable): table of words
+        phone_table (fst.SymbolTable): table of phones
+        state_table (fst.SymbolTable): table of HMM phone-state IDs
+    '''
+    
+    state_table = fst.SymbolTable()
+    phone_table = fst.SymbolTable()
+    word_table = fst.SymbolTable()
+    
+    # add empty <eps> symbol to all tables
+    state_table.add_symbol('<eps>')
+    phone_table.add_symbol('<eps>')
+    word_table.add_symbol('<eps>')
+    
+    for word, phones  in lexicon.items():
+        
+        word_table.add_symbol(word)
+        
+        for p in phones: # for each phone
+            
+            phone_table.add_symbol(p)
+            for i in range(1,n+1): # for each state 1 to n
+                state_table.add_symbol('{}_{}'.format(p, i))
+            
+    return word_table, phone_table, state_table
